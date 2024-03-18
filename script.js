536257fa697023566b44480450e51c9cd4238f87
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-18 09:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(s1,s2){
+        const first = s1.trim().charAt(0);
+        const second = s2.trim().charAt(0);
+        if(first==""||second==""){
+                return "";
+        }
+        return first+second;
 }
+
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
